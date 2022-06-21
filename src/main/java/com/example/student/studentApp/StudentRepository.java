d492746d5a86074105d11a56fd1603cc92f9e905
Herve2iko <ihervedelice@gmail.com>
2022-06-21 12:44:05 +0200

    repository std changes for mod

--- src/main/java/com/example/student/studentApp/StudentRepository.java
+++ src/main/java/com/example/student/studentApp/StudentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
     Optional<Student> findStudentByEmail(String em);
-    Optional<Student> findStudentById(Long id);
+    Student findStudentById(Long id);
     List<Student> findAllById(Long id);
-    
+    //Optional <Student>findById(Long id);
 }
